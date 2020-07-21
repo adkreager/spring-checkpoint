a7c2ce928961878ac1284aace6749c22e4851a6c
Alexis <alexisdkreager@gmail.com>
2020-07-21 09:52:09 -0500

    /camelize

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -1,10 +1,27 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
 
+    @GetMapping("/camelize")
+    public String camelString(@RequestParam("original") String original,
+                           @RequestParam(value = "initialCap", defaultValue = "false") Boolean initialCap) {
+        String[] splitString = original.split("_");
+        String output = "";
+        int startIndex = 0;
 
+        if (!initialCap) {
+            startIndex = 1;
+            output = splitString[0];
+        }
+        for (int i = startIndex; i < splitString.length; i++) {
+            output += splitString[i].substring(0,1).toUpperCase() + splitString[i].substring(1);
+        }
+        return output;
+    }
 
 }
